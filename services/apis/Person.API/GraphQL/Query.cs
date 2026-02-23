56739fbcc366c0c8c7e7f53e385a891a91c0dbfe
Tiziano Crisci <tiziano.tixchim@gmail.com>
2026-02-23 21:14:18 +0100

    -

--- services/apis/Person.API/GraphQL/Query.cs
+++ services/apis/Person.API/GraphQL/Query.cs
@@ -11,12 +11,5 @@ public class Query
     [UseFiltering]
     [UseSorting]
     public IQueryable<PersonModel> GetPerson([Service] PersonContext context) =>
-        context.Person
-            .Include(p => p.PersonDetail).ThenInclude(d => d!.SensibleData)
-            .Include(p => p.NaturalPerson).ThenInclude(n => n!.SensibleData)
-            .Include(p => p.LegalPerson).ThenInclude(l => l!.SensibleData)
-            .Include(p => p.InternalPerson)
-            .Include(p => p.GroupPerson).ThenInclude(g => g!.SensibleData)
-            .Include(p => p.PersonOnlineService)
-            .AsNoTracking();
+        context.Person.AsNoTracking();
 }
